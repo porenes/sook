cd81f12484379ec73bab511ba043d811c54be2f0
Patrice ORENES-LERMA <patrice.orenes-lerma@ledger.fr>
2022-09-15 12:02:08 +0200

    Homepage UI improvement

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import NextLink from "next/link";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import {
   Box,
+  Center,
+  Container,
   Heading,
   Link,
+  SimpleGrid,
   Table,
   TableContainer,
   Tbody,
@@ -16,7 +19,7 @@ import {
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Sook</title>
         <meta name="description" content="Your Tezos NFT Sook" />
@@ -24,29 +27,61 @@ export default function Home() {
       </Head>
 
       <Box>
-        <Heading as="h1">Welcome to Sook</Heading>
-
-        <p>Your ultimate tools for Tezos</p>
-
-        <Box>
-          <NextLink
-            href="/creator/tz1LRugk5K1StypSUpwtRTwkc3J2KriyCNTL"
-            passHref
+        <Center>
+          <Box
+            alignItems="center"
+            justifyContent="space-between"
+            margin="15%"
+            borderRadius="lg"
+            border="1px"
+            p="10"
           >
-            <Link>
-              <Heading as="h2">Creator &rarr;</Heading>
-              <p>Creator tools</p>
-            </Link>
-          </NextLink>
+            <Heading as="h1">Welcome to Sook</Heading>
+            <p>Your ultimate tools for Tezos</p>
+          </Box>
+        </Center>
 
-          <NextLink href="/collector" passHref>
-            <Link>
-              <Heading as="h2">Collectors &rarr;</Heading>
-              <p>Collectors tools</p>
-            </Link>
-          </NextLink>
-        </Box>
+        <SimpleGrid columns={2}>
+          <Box alignItems="center" justifyContent="space-between" margin="5%">
+            <Box
+              p="2"
+              maxW="320px"
+              h="100px"
+              borderWidth="1px"
+              borderRadius="lg"
+            >
+              <NextLink
+                href="/creator/tz1LRugk5K1StypSUpwtRTwkc3J2KriyCNTL"
+                passHref
+              >
+                <Link>
+                  <Heading as="h2">Creator &rarr;</Heading>
+                  <p>Creator tools</p>
+                </Link>
+              </NextLink>
+            </Box>
+          </Box>
+          <Box alignItems="center" justifyContent="space-between" margin="5%">
+            <Box
+              p="2"
+              maxW="320px"
+              h="100px"
+              borderWidth="1px"
+              borderRadius="lg"
+            >
+              <NextLink
+                href="/collector/tz1LRugk5K1StypSUpwtRTwkc3J2KriyCNTL"
+                passHref
+              >
+                <Link>
+                  <Heading as="h2">Collectors &rarr;</Heading>
+                  <p>Collectors tools</p>
+                </Link>
+              </NextLink>
+            </Box>
+          </Box>
+        </SimpleGrid>
       </Box>
-    </div>
+    </Container>
   );
 }
